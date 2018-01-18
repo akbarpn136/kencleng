9a0fb77120e05be9a0f8d9dede69ab3794ada62c
akbarpn136 <akbarpn136@gmail.com>
2018-01-19 06:51:44 +0700

    update aplikasi

--- static/kencleng/src/router.js
+++ static/kencleng/src/router.js
@@ -8,7 +8,7 @@ function load(component) {
     return () => import(`@/${component}.vue`)
 }
 
-export default new VueRouter({
+const router = new VueRouter({
     /*
      * NOTE! VueRouter "history" mode DOESN'T works for Cordova builds,
      * it is only to be used only for websites.
@@ -25,10 +25,26 @@ export default new VueRouter({
     scrollBehavior: () => ({y: 0}),
 
     routes: [
-        {path: '/', component: load('Main'), name: 'main'},
-        {path: '/login', component: load('Login'), name: 'login'},
+        {path: '/', component: load('Main'), name: 'main', meta: {requireAuth: true}},
+        {path: '/login', component: load('Login'), name: 'login', meta: {requireAuth: false}},
 
         // Always leave this last one
-        {path: '*', component: load('Error404')} // Not found
+        {path: '*', component: load('Error404'), name: 'error', meta: {requireAuth: false}}
     ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+    const token = localStorage.getItem('token');
+
+    if (to.matched.some(record => record.meta.requireAuth)) {
+        if (token) {
+            next();
+        } else {
+            next({name: 'login'});
+        }
+    } else {
+        next();
+    }
+});
+
+export default router;
